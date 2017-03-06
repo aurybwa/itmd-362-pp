15906ddb8cef6c9d7f8193e3d89bcae6adb29bed
Aury <abwashi@hawk.iit.edu>
2017-03-06 15:15:15 -0600

    Included webfontloader. Finished pp-04.

--- pp-04/js/site.js
+++ pp-04/js/site.js
@@ -3,6 +3,12 @@ $.noConflict();
 
 // jQuery 3.x-style ready event and locally scoped $
 jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
+	$('html').removeClass('nojs');
+	$('html').addClass('hasjs');
+});
+
+WebFont.load({
+	google: {
+		families: [ 'Lato']
+	}
 });
